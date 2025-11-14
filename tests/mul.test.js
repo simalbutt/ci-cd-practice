c40ba5368f09e464e8b275f97ba0b245f4635049
simalbutt <138391970+simalbutt@users.noreply.github.com>
2025-11-14 11:09:07 +0500

    add mocking practice

--- tests/mul.test.js
+++ tests/mul.test.js
@@ -1,6 +1,8 @@
 // tests/multiply.test.js
 const request = require('supertest');
 const app = require('../index');
+const nock = require('nock');
+const { fetchData } = require('../lib/external');
 
 describe('API Tests', () => {
   test('GET /health', async () => {
@@ -19,6 +21,14 @@ describe('API Tests', () => {
   expect(res.status).toBe(200);
   expect(res.body.result).toBe(12);
 });
+
+test('fetchData returns data from API', async () => {
+  nock('https://api.example.com').get('/').reply(200, { hello: 'world' });
+  const data = await fetchData('https://api.example.com/');
+  expect(data).toEqual({ hello: 'world' });
+  nock.cleanAll();
+});
+
 });
 
 
